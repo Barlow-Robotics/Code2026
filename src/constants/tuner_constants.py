63a991cbe1b9437346b98c213419ae7c1d94ea96
Brian Weiss <113956118+BrianWeiss1@users.noreply.github.com>
2026-03-21 14:25:50 -0400

    increased current limit

--- src/constants/tuner_constants.py
+++ src/constants/tuner_constants.py
@@ -63,7 +63,7 @@ class TunerConstants:
     # Some configs will be overwritten; check the `with_*_initial_configs()` API documentation.
     _drive_initial_configs = configs.TalonFXConfiguration().with_current_limits(
         configs.CurrentLimitsConfigs()
-        .with_supply_current_limit(40.0)
+        .with_supply_current_limit(60.0)
         .with_supply_current_limit_enable(True)
         # Swerve azimuth does not require much torque output, so we can set a relatively low
         # stator current limit to help avoid brownouts without impacting performance.
@@ -73,7 +73,7 @@ class TunerConstants:
     )
     _steer_initial_configs = configs.TalonFXConfiguration().with_current_limits(
         configs.CurrentLimitsConfigs()
-        .with_supply_current_limit(40.0)
+        .with_supply_current_limit(60.0)
         .with_supply_current_limit_enable(True)
         # Swerve azimuth does not require much torque output, so we can set a relatively low
         # stator current limit to help avoid brownouts without impacting performance.
