7793926d2589505a2afab1049d3192ec7a8a2afb
Michael Belanger <magicmikejr3email@gmail.com>
2026-03-28 16:28:39 -0400

    use intake retract in auto

--- src/commands/go_to_position_command.py
+++ src/commands/go_to_position_command.py
@@ -27,8 +27,8 @@ class IntakePositionCommand(Command):
     def initialize(self):
         # current_speeds = self.drive_sub.get_speeds()
         # overall_velocity = (current_speeds.vx**2 + current_speeds.vy**2) ** 0.5
-        if self.position != IntakePositions.HOME or not self.auto:
-            self.intake_sub.go_to_position(self.position, 0)
+        # if self.position == IntakePositions.HOME and self.auto:
+        self.intake_sub.go_to_position(self.position, 0)
 
     def execute(self):
         if self.auto:
