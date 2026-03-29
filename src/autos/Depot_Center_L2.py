26b1a7fd5ce6b77422b126033af3f94c58c977d6
Brian Weiss <113956118+BrianWeiss1@users.noreply.github.com>
2026-03-28 20:11:30 -0400

    Added depot side auto

--- src/autos/Depot_Center_L2.py
+++ src/autos/Depot_Center_L2.py
@@ -14,13 +14,13 @@ if typing.TYPE_CHECKING:
 from commands import IntakePositionCommand
 from subsystems import IntakePositions
 
-path_name = "Depot_Center_2L"
+path_name = "HP_Center_2L"
 
 
 class Depot_Center_L2:
     def __init__(self, container: "RobotContainer"):
         self.paths = [
-            PathPlannerPath.fromChoreoTrajectory(path_name, i) for i in range(6)
+            PathPlannerPath.fromChoreoTrajectory(path_name, i).mirrorPath() for i in range(6)
         ]
         self.container = container
 
@@ -36,6 +36,7 @@ class Depot_Center_L2:
                     self.container.intake,
                     IntakePositions.DEPLOYED,
                     auto=True,
+
                 ).withTimeout(3),
             ),
             ParallelDeadlineGroup(
